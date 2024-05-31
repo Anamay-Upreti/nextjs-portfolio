441cb2ea93b0af91ab072361c3e5d4724d3bfbb6
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-05-31 23:30:32 +0530

    started-bentogrid

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
 import Image from "next/image";
+import { HiHomeModern } from "react-icons/hi2";
 
 export default function Home() {
   return (
     <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 ">
            <div className="max-w-7xl w-full">
+            <FloatingNav navItems={[{name: 'Home', link: '/', icon: <HiHomeModern/>}]} />
             <Hero/>
+            <Grid/>
            </div>
     </main>
   );
