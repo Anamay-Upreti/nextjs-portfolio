b00aa2123c58a9b0e69ca72da2e1162d35c3232c
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-06-03 17:24:40 +0530

    added-3danimation-cards

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,6 +1,7 @@
 import { projects } from '@/data'
 import React from 'react'
 import { div } from 'three/examples/jsm/nodes/Nodes.js'
+import { PinContainer } from './ui/3d-pin'
 
 const RecentProjects = () => {
   return (
@@ -11,8 +12,12 @@ const RecentProjects = () => {
         </h1>
         <div className='flex flex-wrap items-center justify-center p-4 gap-16 mt-10'>
          {projects.map(({id, title, des, img, iconLists, link, }) => (
-            <div>
-                {project.title}
+            <div key={id}
+            className='lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]'
+            >
+               <PinContainer>
+                {title}
+                </PinContainer>
             </div>
          ))}
         </div>
