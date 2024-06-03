aae3435f9c8b8cda0c6fdbb8fbeded5110e8d337
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-06-03 19:24:34 +0530

    added-imgs etc

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -2,6 +2,8 @@ import { projects } from '@/data'
 import React from 'react'
 import { div } from 'three/examples/jsm/nodes/Nodes.js'
 import { PinContainer } from './ui/3d-pin'
+import { TiLocationArrow } from 'react-icons/ti'
+import { FaLocationArrow } from 'react-icons/fa6'
 
 const RecentProjects = () => {
   return (
@@ -15,8 +17,49 @@ const RecentProjects = () => {
             <div key={id}
             className='lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]'
             >
-               <PinContainer>
-                {title}
+               <PinContainer title={title} href={link}>
+                <div className='relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10'>
+                  <div className='relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]'>
+                    <img
+                    src='/bg.png'
+                    alt='bg'
+                    />
+                  </div>
+                  <img src={img} alt={title} 
+                  className='z-10 absolute bottom-0'
+                  />
+                </div>
+                <h1 className='font-bold lg:text-2xl md:text-xl text-base line-clamp-1'>
+
+                  {title}
+                </h1>
+                <p className='lg:text-xl lg:font-normal font-light text-sm line-clamp-2'>
+                  {des}
+                </p>
+
+               <div className='flex items-center justify-between mt-7 mb-3'>
+              <div className='flex items-center'>
+                 {iconLists.map((icon, index)=>(
+                  <div key={icon} className='border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center' style={{transform: `translateX(-${5 * index * 2}px)`}}>
+                   <img 
+                   src={icon}
+                   alt={icon}
+                   className='p-2'
+                   />
+                  </div>
+                 ))}
+                </div>
+
+                <div className="flex justify-center items-center"></div>
+               <div>
+                <p className='flex lg:text-xl md:text-xs text-sm text-purple'>Check Live Site</p>
+                <FaLocationArrow
+                className='ms-3'
+                color='#CBACF9'
+                />
+               </div>
+               </div>
+
                 </PinContainer>
             </div>
          ))}
