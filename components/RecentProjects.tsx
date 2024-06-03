5286cb752839c6ebc333a5f36e61be447006017c
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-06-03 20:06:26 +0530

    added-navbar

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -7,7 +7,7 @@ import { FaLocationArrow } from 'react-icons/fa6'
 
 const RecentProjects = () => {
   return (
-    <div className='py-20'>
+    <div className='py-20' id="projects">
         <h1 className='heading'>
             A small Selection of {' '}
             <span className='text-purple'>Recent Projects</span>
